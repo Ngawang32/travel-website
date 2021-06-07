2e62855d364530a2fa13f016a2d71ebc77e68164
Ngawang32 <ngawang32@hotmail.com>
2021-06-07 16:03:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const hotels = document.getElementById('hotels');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+const data = [
+    {   id:1,
+        image: './images/hotel-1.jpg',
+
+    }
+]
+
+right.addEventListener('click', () => {
+    
+});
